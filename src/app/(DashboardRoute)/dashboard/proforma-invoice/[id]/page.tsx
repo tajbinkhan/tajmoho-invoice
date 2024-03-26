cd0de286b7c56f9f016d39f0136a34da90f96456
Tajbin Khan <tajbink@gmail.com>
2024-03-26 22:52:48 +0600

    Add metadata for Proforma Invoice update page

--- src/app/(DashboardRoute)/dashboard/proforma-invoice/[id]/page.tsx
+++ src/app/(DashboardRoute)/dashboard/proforma-invoice/[id]/page.tsx
@@ -1,4 +1,10 @@
 import ProformaInvoiceUpdateFormTemplateView from "@/modules/ProformaInvoice/Templates/Form/ProformaInvoiceUpdateFormTemplateView";
+import { Metadata } from "next";
+import { siteTitle } from "@/core/Helpers";
+
+export const metadata: Metadata = {
+	title: `Update Proforma Invoice - ${siteTitle}`
+};
 
 export default function ProformaInvoiceUpdate({ params }: { params: { id: string } }) {
 	return <ProformaInvoiceUpdateFormTemplateView id={params.id} />;
